24d5170f6411688f879e6a3ee1e5a01b10c82ba2
bensonitguy <bensonitguy@gmail.com>
2019-02-26 11:16:01 +0530

    added renderlist function to render the element after response

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,10 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder.js';
 
 export const fetchPosts = () =>  {
-    return async(dispatch) => {
+    return async dispatch => {
     
         const response = await jsonPlaceholder.get('/posts');
-        dispatch( {type: 'FETCH_POSTS', payload : response} );
+        dispatch( {type: 'FETCH_POSTS', payload : response.data} );
 
     };
 };
